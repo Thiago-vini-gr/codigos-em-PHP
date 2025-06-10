7e9f09f74ea6324bfc66ddb3e4ebe6fcebbba274
Thiago-vini-gr <thiagaogamerrj@gmail.com>
2025-06-10 08:18:14 -0300

    Add initial PHP variable demonstration in codigo.php and index.html

---
+++ codigo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variaveis em php</title>
+</head>
+<body>
+    <h1>variaveis em php</h1>
+    <?php
+        $valor = "André";
+        echo %valor. "<br><br>";
+
+        $valor = 9;
+        $valor = $valor + 1;
+        echo $valor;
+    ?>
+</body>
+</html>
