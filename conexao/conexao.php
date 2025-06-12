3df0b78da9e3032674448fbe462d4700df1e6845
thiago-vini-gr <thiagaogamerrj@gmail.com>
2025-06-12 09:16:28 -0300

    Remove unused image files and add new PHP examples demonstrating database connection, control structures, and functions

---
+++ conexao/conexao.php
@@ -0,0 +1,16 @@
+<?php
+$hostname = "localhost";
+$bancodedados = "banco_php";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+    if ($mysqli->connect_error) {
+    echo "Falha na conexão:( " . $mysqli->connect_error . ")" . $mysqli->connect_errno;
+} 
+    else {
+    echo "Conexão bem-sucedida!";
+}
+
+?>
